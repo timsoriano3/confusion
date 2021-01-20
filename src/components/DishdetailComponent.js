ba20c6cbf9430d51cbd166c1a53612665b9121d6
Peter Tim Soriano <chops10@my.yorku.ca>
2021-01-20 18:25:58 -0500

    redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,23 +5,65 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 
-const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
+
+function RenderDish({dish}) {
+    if (dish != null) {
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    } else {
+        return(
+            <div></div>
+        );
+    }
+}
+
+function RenderComments({comments, addComment, dishId}) {
+    if (comments != null)
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {comments.map((comment) => {
+                        return(
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <CommentForm dishId={dishId} addComment={addComment} />
+            </div>
+        );
+    else
+        return(
+            <div></div>
+        );  
+}
+
 
 class CommentForm extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {
-            isModalOpen: false
-        };
         this.toggleModal = this.toggleModal.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
-    }
-    handleSubmit(values) {
-        alert("Comment: " + JSON.stringify(values));
+        
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
     }
     
     toggleModal() {
@@ -29,10 +71,15 @@ class CommentForm extends Component {
             isModalOpen: !this.state.isModalOpen
         });
     }
+    
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
 
     render() {
         return(
-            <React.Fragment>
+            <div>
                 <Button outline onClick={this.toggleModal}><span className="fa fa-pencil"></span> Submit Comment</Button>
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>
@@ -59,7 +106,6 @@ class CommentForm extends Component {
                                 placeholder="Your Name"
                                 className="form-control"
                                 validators={{
-                                    required,
                                     maxLength: maxLength(15),
                                     minLength: minLength(3)
                                 }}
@@ -68,7 +114,6 @@ class CommentForm extends Component {
                                 className="text-danger" model=".author"
                                 show="touched"
                                 message={{
-                                    required: 'Required',
                                     minLength: 'Must be greater than 2 characters',
                                     maxLength: 'Must be 15 characters or less'
                                 }}
@@ -76,7 +121,7 @@ class CommentForm extends Component {
                             </FormGroup>
                             <FormGroup>
                                 <Label htmlFor="comment">Comment</Label>
-                                <Control.textarea model="comment"
+                                <Control.textarea model=".comment"
                                 id="comment" name="comment"
                                 className="form-control" rows="6"
                                 />
@@ -89,51 +134,11 @@ class CommentForm extends Component {
                         </LocalForm>
                     </ModalBody>
                 </Modal>
-            </React.Fragment>
-        );
-    }
-}
-
-function RenderDish({dish}) {
-    if (dish != null) {
-        return(
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        );
-    } else {
-        return(
-            <div></div>
-        );
-    }
-}
-
-function RenderComments({comments}) {
-    if (comments != null)
-        return(
-            <div>
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {comments.map((comment) => {
-                        return(
-                            <li key={comment.id}>
-                                <p>{comment.comment}</p>
-                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                            </li>
-                        );
-                    })}
-                </ul>
             </div>
         );
-    else
-        return(
-            <div></div>
-        );  
+    }
 }
+
 const DishDetail = (props) => {
     if (props.dish != null) {
         return (
@@ -143,19 +148,16 @@ const DishDetail = (props) => {
                         <BreadcrumbItem active><Link to='/menu'>Menu</Link></BreadcrumbItem>
                         <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
                     </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
                 </div>
-            </div>
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
-                        <CommentForm />
-                    </div>
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
                 </div>
             </div>
         );
@@ -166,5 +168,6 @@ const DishDetail = (props) => {
         );
     }
 }
+
 
 export default DishDetail;
